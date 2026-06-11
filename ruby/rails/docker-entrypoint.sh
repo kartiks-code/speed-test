05ed6b78a5a5e8f128a87bdad06d4d0291396f9c
kartik-kulkarni <kartik.kulkarni.2002@gmail.com>
2026-06-10 22:07:45 -0400

    docker fixes

--- ruby/rails/docker-entrypoint.sh
+++ ruby/rails/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'bin/rails' ] && [ "$2" = 's' ]; then
-    rm -f /myapp/tmp/pids/server.pid
+if [ "$1" = 'bin/rails' ] && { [ "$2" = 'server' ] || [ "$2" = 's' ]; }; then
+    rm -f tmp/pids/server.pid
     bin/rails db:migrate
 fi
 
